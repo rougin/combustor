f3775321fc39a63e1d87d8986ba1d5a08a349da5
Rougin Gutib <rougingutib@gmail.com>
2014-09-13 07:24:05 +0800

    Modified command line interface

--- bin/combustor.php
+++ bin/combustor.php
@@ -6,12 +6,16 @@
 
 (@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
+define('VENDOR', str_replace('rougin/combustor/bin', '', __DIR__));
+
 /**
  * Import the commands from Combuster
  */
 
 use Combustor\CreateControllerCommand;
 use Combustor\CreateModelCommand;
+use Combustor\Doctrine\ModifyCommand;
+use Combustor\Doctrine\RevertCommand;
 
 /**
  * Import the Symfony Console Component
@@ -23,4 +27,6 @@ use Symfony\Component\Console\Helper\HelperSet;
 $application = new Application('Combustor', '1');
 $application->add(new CreateControllerCommand);
 $application->add(new CreateModelCommand);
+$application->add(new ModifyCommand);
+$application->add(new RevertCommand);
 $application->run();

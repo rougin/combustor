78ec9d30d5a76171ad2ad76192e05cf98edb7181
Rougin Gutib <rougingutib@gmail.com>
2014-09-15 14:11:34 +0800

    Added functionalities

--- src/Combustor/CreateControllerCommand.php
+++ src/Combustor/CreateControllerCommand.php
@@ -3,6 +3,7 @@
 namespace Combustor;
 
 use Inflect\Inflect;
+use Combustor\Tools\GetColumns;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,6 +13,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CreateControllerCommand extends Command
 {
 
+	/**
+	 * Set the configurations of the specified command
+	 */
 	protected function configure()
 	{
 		$this->setName('create:controller')
@@ -25,21 +29,144 @@ class CreateControllerCommand extends Command
 				null,
 				InputOption::VALUE_NONE,
 				'Keeps the name to be used'
-			)->addOption(
-				'empty',
-				null,
-				InputOption::VALUE_NONE,
-				'Generate a controller with no CRUD'
 			);
 	}
 
+	/**
+	 * Execute the command
+	 * 
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface $output
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$name = ($input->getOption('keep')) ? $input->getArgument('name') : Inflect::pluralize($input->getArgument('name'));
-
+		/**
+		 * Get the controller template
+		 */
+		
 		$controller = file_get_contents(__DIR__ . '/Templates/Controller.txt');
+		
+		/**
+		 * Get the columns from the specified name
+		 */
 
-		$output->writeln($name);
+		$columns = new GetColumns($input->getArgument('name'), $output);
+
+		if ( ! $columns->result()) {
+			$output->writeln('<error>There is no table named "' . $input->getArgument('name') . '" from the database!</error>');
+			exit();	
+		}
+
+		$columns = $columns->result();
+
+		$columnsCreate = NULL;
+		$columnsEdit = NULL;
+		$columnsValidate = NULL;
+		$counter = 0;
+
+		foreach ($columns as $row) {
+			if ($counter != 0) {
+				if ($row->Field != 'datetime_updated') {
+					$columnsCreate .= "			";
+				}
+
+				if ($row->Field != 'datetime_created') {
+					$columnsEdit .= "			";
+				}
+
+				if ($row->Field != 'password' && $row->Field != 'datetime_created' && $row->Field != 'datetime_updated') {
+					$columnsValidate .= "			";
+				}
+			}
+
+			if ($row->Extra == 'auto_increment') {
+				continue;
+			} elseif ($row->Key == 'MUL') {
+				$entity = str_replace('_id', '', $row->Field);
+				$columnsCreate .= "\n" . '			$' . $entity . ' = $this->doctrine->em->find(\'' . $entity . '\', $this->input->post(\'' . $row->Field . '\'));' . "\n";
+				$columnsCreate .= '			$this->$singular->set_' . strtolower($row->Field) . '($' . $entity . ');';
+
+				$columnsEdit .= "\n" . '			$' . $entity . ' = $this->doctrine->em->find(\'' . $entity . '\', $this->input->post(\'' . $row->Field . '\'));' . "\n";
+				$columnsEdit .= '			$$singular->set_' . strtolower($row->Field) . '($' . $entity . ');';
+			} elseif ($row->Field == 'password') {
+				$columnsCreate .= "\n" . '			if ($this->input->post(\'password\') == $this->input->post(\'confirm_password\')) {' . "\n";
+				$columnsCreate .= '				$this->$singular->set_password($this->input->post(\'password\'));' . "\n";
+				$columnsCreate .= '			} else {' . "\n";
+				$columnsCreate .= '				$this->session->set_flashdata(\'notification\', \'The passwords you entered did not match!\');' . "\n";
+				$columnsCreate .= '				$this->session->set_flashdata(\'alert\', \'danger\');' . "\n";
+				$columnsCreate .= '				redirect(\'$plural/create\');' . "\n";
+				$columnsCreate .= '			}' . "\n\n";
+
+				$columnsEdit .= "\n" . '			if ($this->input->post(\'old_password\') != NULL && $this->input->post(\'new_password\') != NULL && $this->input->post(\'confirm_password\') != NULL) {' . "\n";
+				$columnsEdit .= '				if (md5($this->input->post(\'old_password\')) != $$singular->get_password() || $this->input->post(\'new_password\') != $this->input->post(\'confirm_password\')) {' . "\n";
+				$columnsEdit .= '					$this->session->set_flashdata(\'notification\', \'The passwords you entered did not match!\');' . "\n";
+				$columnsEdit .= '					$this->session->set_flashdata(\'alert\', \'danger\');' . "\n";
+				$columnsEdit .= '					redirect(\'$plural/edit/\' . $id);' . "\n";
+				$columnsEdit .= '				} else {' . "\n";
+				$columnsEdit .= '					$$singular->set_password($this->input->post(\'new_password\'));' . "\n";
+				$columnsEdit .= '				}' . "\n";
+				$columnsEdit .= '			}' . "\n\n";
+			} else {
+				$column = ($row->Field == 'datetime_created' || $row->Field == 'datetime_updated') ? 'now' : $row->Field;
+
+				if ($row->Field != 'datetime_updated') {
+					$columnsCreate .= '$this->$singular->set_' . strtolower($row->Field) . '($this->input->post(\'' . $column . '\'));' . "\n";
+				}
+
+				if ($row->Field != 'datetime_created') {
+					$columnsEdit .= '$$singular->set_' . strtolower($row->Field) . '($this->input->post(\'' . $column . '\'));' . "\n";
+				}
+			}
+
+			if ($row->Field != 'password' && $row->Field != 'datetime_created' && $row->Field != 'datetime_updated') {
+				$columnsValidate .= '\'' . $row->Field . '\' => \'' . str_replace('_', ' ', $row->Field) . '\',' . "\n";
+			}
+
+			$counter++;
+		}
+
+		/**
+		 * Search and replace the following keywords from the template
+		 */
+
+		$search = array(
+			'$columnsCreate',
+			'$columnsEdit',
+			'$columnsValidate',
+			'$controller',
+			'$plural',
+			'$singular'
+		);
+
+		$replace = array(
+			rtrim($columnsCreate),
+			rtrim($columnsEdit),
+			rtrim($columnsValidate),
+			ucfirst(Inflect::pluralize($input->getArgument('name'))),
+			Inflect::pluralize($input->getArgument('name')),
+			Inflect::singularize($input->getArgument('name'))
+		);
+
+		$controller = str_replace($search, $replace, $controller);
+
+		/**
+		 * Create a new file and insert the generated template
+		 */
+
+		$name = ($input->getOption('keep')) ? Inflect::pluralize($input->getArgument('name')) : $input->getArgument('name');
+
+		$filename = APPPATH . 'controllers/' . ucfirst($name) . '.php';
+
+		if (file_exists($filename)) {
+			$output->writeln('<error>The ' . Inflect::pluralize($input->getArgument('name')) . ' controller already exists!</error>');
+
+			exit();
+		}
+
+		$file = fopen($filename, 'wb');
+		file_put_contents($filename, $controller);
+
+		$output->writeln('<info>The controller "' . Inflect::pluralize($input->getArgument('name')) . '" has been created successfully!</info>');
 	}
 	
 }
--- src/Combustor/CreateModelCommand.php
+++ src/Combustor/CreateModelCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace Combustor;
 
+use Inflect\Inflect;
+use Combustor\Tools\GetColumns;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,6 +12,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CreateModelCommand extends Command
 {
 
+	/**
+	 * Set the configurations of the specified command
+	 */
 	protected function configure()
 	{
 		$this->setName('create:model')
@@ -19,15 +24,239 @@ class CreateModelCommand extends Command
 				InputArgument::REQUIRED,
 				'Name of the model'
 			)->addOption(
-				'keep',
+				'snake',
 				null,
 				InputOption::VALUE_NONE,
-				'Keeps the name to be used'
+				'Use the snake case naming convention for the accessor and mutators'
 			);
 	}
 
+	/**
+	 * Get the data type of the specified column
+	 * 
+	 * @param  string $type
+	 * @return string
+	 */
+	protected function getDataType($type)
+	{
+		if (strpos($type, 'array') !== FALSE) $type = 'array';
+		elseif (strpos($type, 'bigint') !== FALSE) $type = 'bigint';
+		elseif (strpos($type, 'blob') !== FALSE) $type = 'blob';
+		elseif (strpos($type, 'boolean') !== FALSE) $type = 'boolean';
+		elseif (strpos($type, 'datetime') !== FALSE || strpos($type, 'timestamp') !== FALSE) $type = 'datetime';
+		elseif (strpos($type, 'datetimetz') !== FALSE) $type = 'datetimetz';
+		elseif (strpos($type, 'date') !== FALSE) $type = 'date';
+		elseif (strpos($type, 'decimal') !== FALSE) $type = 'decimal';
+		elseif (strpos($type, 'float') !== FALSE) $type = 'float';
+		elseif (strpos($type, 'guid') !== FALSE) $type = 'guid';
+		elseif (strpos($type, 'int') !== FALSE) $type = 'integer';
+		elseif (strpos($type, 'json_array') !== FALSE) $type = 'json_array';
+		elseif (strpos($type, 'object') !== FALSE) $type = 'object';
+		elseif (strpos($type, 'simple_array') !== FALSE) $type = 'simple_array';
+		elseif (strpos($type, 'smallint') !== FALSE) $type = 'smallint';
+		elseif (strpos($type, 'text') !== FALSE) $type = 'text';
+		elseif (strpos($type, 'time') !== FALSE) $type = 'time';
+		elseif (strpos($type, 'varchar') !== FALSE) $type = 'string';
+
+		return $type;
+	}
+	
+	/**
+	 * Execute the command
+	 * 
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface $output
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		/**
+		 * Get the model template
+		 */
+		
+		$model = file_get_contents(__DIR__ . '/Templates/Model.txt');
+		
+		/**
+		 * Get the columns from the specified name
+		 */
+
+		$databaseColumns = new GetColumns($input->getArgument('name'), $output);
+
+		if ( ! $databaseColumns->result()) {
+			$output->writeln('<error>There is no table named "' . $input->getArgument('name') . '" from the database!</error>');
+
+			exit();	
+		}
+
+		$databaseColumns = $databaseColumns->result();
+
+		$accessors = NULL;
+		$columns = NULL;
+		$counter = 0;
+		$indexCounter = 0;
+		$mutatorCounter = 0;
+		$indexes = NULL;
+		$mutators = NULL;
+
+		foreach ($databaseColumns as $row) {
+			$nullable = ($row->Null == 'YES') ? 'TRUE' : 'FALSE';
+
+			$type = $this->getDataType($row->Type);
+
+			/**
+			 * The data type is an integer or string? Set the length of the specified data type
+			 */
+
+			if ((strpos($row->Type, 'int') !== FALSE) || (strpos($row->Type, 'varchar') !== FALSE)) {
+				$length =  ', length=' . str_replace(array($type, '(', ')', 'varchar', 'int'), array('', '', '', '', ''), $row->Type);
+			} else {
+				$length = NULL;
+			}
+
+			$unique = ($row->Key == 'UNI') ? 'TRUE' : 'FALSE';
+
+			if ($counter != 0) {
+				$accessors .= '	';
+				$columns .= '	';
+			}
+
+			if ($indexCounter != 0) {
+				$indexes .= '*   		';
+			}
+
+			if ($mutatorCounter != 0) {
+				$mutators .= '	';
+			}
+
+			/**
+			 * Generate the columns and indexes
+			 */
+
+			$columns .= '/**' . "\n";
+
+			if ($row->Key == 'PRI') {
+				$columns .= '	 * @Id @GeneratedValue' . "\n";
+				$columns .= '	 * @Column(type="' . $type . '"' . $length . ', nullable=' . $nullable . ', unique=' . $unique . ')' . "\n";
+			} elseif ($row->Key == 'MUL') {
+				$indexCounter++;
+
+				$entity = ucfirst(str_replace('_id', '', $row->Field));
+				$indexes .= '@index(name="' . $row->Field . '", columns={"' . $row->Field . '"}),' . "\n";
+				$type = '\\' . ucfirst($entity);
+
+				$columns .= '	 * @ManyToOne(targetEntity="' . $entity . '")' . "\n";
+				$columns .= '	 * @JoinColumns({' . "\n";
+				$columns .= '	 *   @JoinColumn(name="' . $row->Field . '", referencedColumnName="' . $row->Field . '", nullable=' . $nullable . ', onDelete="cascade")' . "\n";
+				$columns .= '	 * })' . "\n";
+			} else {
+				$columns .= '	 * @Column(type="' . $type . '"' . $length . ', nullable=' . $nullable . ', unique=' . $unique . ')' . "\n";
+			}
+
+			$columns .= '	 */' . "\n";
+			$columns .= '	protected $' . $row->Field . ';' . "\n\n";
+
+			/**
+			 * Generate the accessors
+			 */
+
+			$methodName = 'get_' . $row->Field;
+
+			if ($input->getOption('snake')) {
+				$methodName = preg_replace('/[\s]+/', '_', trim(strtolower($methodName)));
+			} else {
+				$methodName = strtolower($methodName[0]).substr(str_replace(' ', '', ucwords(preg_replace('/[\s_]+/', ' ', $methodName))), 1);
+			}
+
+			$accessors .= '/**' . "\n";
+			$accessors .= '	 * Get ' . $row->Field . "\n";
+			$accessors .= '	 *' . "\n";
+			$accessors .= '	 * @return ' . $type . "\n";
+			$accessors .= '	 */' . "\n";
+			$accessors .= '	public function ' . $methodName . '()' . "\n";
+			$accessors .= '	{' . "\n";
+			$accessors .= '		return $this->' . $row->Field . ';' . "\n";
+			$accessors .= '	}' . "\n\n";
+
+			/**
+			 * Generate the mutators
+			 */
+
+			if ($row->Key != 'PRI') {
+				$nullable = ($row->Null == 'YES') ? ' = NULL' : NULL;
+
+				$mutatorCounter++;
+
+				if ($row->Key == 'MUL') {
+					$class = ucfirst(str_replace('_id', '', $row->Field));
+				} elseif ($this->getDataType($row->Type) == 'date' || $this->getDataType($row->Type) == 'datetime' || $this->getDataType($row->Type) == 'datetimetz') {
+					$class = 'DateTime';
+				} else {
+					$class = '$model';
+				}
+
+				$methodName = 'set_' . $row->Field;
+
+				if ($input->getOption('snake')) {
+					$methodName = preg_replace('/[\s]+/', '_', trim(strtolower($methodName)));
+				} else {
+					$methodName = strtolower($methodName[0]).substr(str_replace(' ', '', ucwords(preg_replace('/[\s_]+/', ' ', $methodName))), 1);
+				}
+
+				$mutators .= '/**' . "\n";
+				$mutators .= '	 * Set ' . $row->Field . "\n";
+				$mutators .= '	 *' . "\n";
+				$mutators .= '	 * @param ' . $type . ' $' . $row->Field . "\n";
+				$mutators .= '	 * @return ' . $class . "\n";
+				$mutators .= '	 */' . "\n";
+				$mutators .= '	public function ' . $methodName . '(\\' . $class . ' $' . $row->Field . $nullable . ')' . "\n";
+				$mutators .= '	{' . "\n";
+				$mutators .= '		$this->' . $row->Field . ' = $' . $row->Field . ';' . "\n\n";
+				$mutators .= '		return $this;' . "\n";
+				$mutators .= '	}' . "\n\n";
+			}
+
+			$counter++;
+		}
+
+		/**
+		 * Search and replace the following keywords from the template
+		 */
+
+		$search = array(
+			'$indexes',
+			'$columns',
+			'$accessors',
+			'$mutators',
+			'$singular',
+			'$model'
+		);
+
+		$replace = array(
+			rtrim($indexes),
+			rtrim($columns),
+			rtrim($accessors),
+			rtrim($mutators),
+			Inflect::singularize($input->getArgument('name')),
+			ucfirst(Inflect::singularize($input->getArgument('name')))
+		);
+
+		$model = str_replace($search, $replace, $model);
+
+		/**
+		 * Create a new file and insert the generated template
+		 */
+
+		$filename = APPPATH . 'models/' . ucfirst(Inflect::singularize($input->getArgument('name'))) . '.php';
+
+		if (file_exists($filename)) {
+			$output->writeln('<error>The ' . Inflect::singularize($input->getArgument('name')) . ' model already exists!</error>');
+			
+			exit();
+		}
+
+		$file = fopen($filename, 'wb');
+		file_put_contents($filename, $model);
+
+		$output->writeln('<info>The model "' . Inflect::singularize($input->getArgument('name')) . '" has been created successfully!</info>');
 	}
 	
 }

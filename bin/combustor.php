53720e520077459473e9a05a93186ce7e8f84a99
Rougin Royce Gutib <rougingutib@gmail.com>
2016-04-05 00:23:26 +0800

    Update combustor.php

--- bin/combustor.php
+++ bin/combustor.php
@@ -1,30 +1,49 @@
 <?php
 
-// Load the Blueprint library
-$combustor = new Rougin\Blueprint\Blueprint(
-    new Symfony\Component\Console\Application,
-    new Auryn\Injector
-);
+// Include the Composer Autoloader
+require 'vendor/autoload.php';
 
-$combustor
-    ->setTemplatePath(__DIR__.'/../src/Templates')
-    ->setCommandPath(__DIR__.'/../src/Commands')
+// Load the Blueprint library
+$injector = new Auryn\Injector;
+$consoleApp = new Symfony\Component\Console\Application;
+$app = new Rougin\Blueprint\Blueprint($consoleApp, $injector);
+
+// Application details
+$app->console->setName('Combustor');
+$app->console->setVersion('1.2.0');
+
+$app
+    ->setTemplatePath(__DIR__ . '/../src/Templates')
+    ->setCommandPath(__DIR__ . '/../src/Commands')
     ->setCommandNamespace('Rougin\Combustor\Commands');
 
-$combustor->console->setName('Combustor');
-$combustor->console->setVersion('1.1.4');
-
-$combustor->injector->delegate('CI_Controller', function() {
+$app->injector->delegate('CI_Controller', function () {
     $sparkPlug = new Rougin\SparkPlug\SparkPlug($GLOBALS, $_SERVER);
 
     return $sparkPlug->getCodeIgniter();
 });
 
-$combustor->injector->delegate('Rougin\Describe\Describe', function() use ($db) {
-    return new Rougin\Describe\Describe(
-        new Rougin\Describe\Driver\CodeIgniterDriver($db)
-    );
+$ci = $app->injector->make('CI_Controller');
+
+$app->injector->delegate('Rougin\Describe\Describe', function () use ($ci) {
+    $ci->load->database();
+
+    $config['default'] = [
+        'dbdriver' => $ci->db->dbdriver,
+        'hostname' => $ci->db->hostname,
+        'username' => $ci->db->username,
+        'password' => $ci->db->password,
+        'database' => $ci->db->database
+    ];
+
+    if (empty($config['default']['hostname'])) {
+        $config['default']['hostname'] = $ci->db->dsn;
+    }
+
+    $driver = new Rougin\Describe\Driver\CodeIgniterDriver($config);
+
+    return new Rougin\Describe\Describe($driver);
 });
 
 // Run the Combustor console application
-$combustor->run();
+$app->run();

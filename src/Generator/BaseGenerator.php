860ab7e2b5c89320a704fb53df855a3810c00a4c
Rougin Gutib <rougingutib@gmail.com>
2019-05-17 13:56:58 +0800

    Update BaseGenerator

--- src/Generator/BaseGenerator.php
+++ src/Generator/BaseGenerator.php
@@ -8,7 +8,7 @@ use Rougin\Describe\Describe;
  * Base Generator
  *
  * @package Combustor
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class BaseGenerator
 {

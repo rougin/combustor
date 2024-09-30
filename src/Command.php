ff85daf1c0d750003fddc067cc6e1a875eae1c9a
Rougin Gutib <rougingutib@gmail.com>
2024-09-30 19:44:42 +0800

    Change details in Command class

--- src/Command.php
+++ src/Command.php
@@ -48,34 +48,19 @@ class Command extends Blueprint
     }
 
     /**
-     * Configures the current command.
-     *
      * @return void
      */
     public function init()
     {
         $this->addArgument('name', 'Name of the table');
 
-        // TODO: Add Bootstrap styling in the generated views -------------
-        // if ($this->name === 'create:layout')
-        // {
-        //     $this->addOption('bootstrap', 'Includes Bootstrap styling');
-        // }
-        // ----------------------------------------------------------------
-
-        $this->addOption('camel', 'Uses the camelCase convention');
-
-        $this->addOption('keep', 'Keeps the name to be used');
-
-        if ($this->name === 'create:controller')
+        if ($this->name !== 'create:views')
         {
-            $this->addOption('lowercase', 'Keeps the first character of the name to lowercase');
+            $this->addOption('keep', 'Do not inflect the class name');
         }
     }
 
     /**
-     * Checks whether the command is enabled or not in the current environment.
-     *
      * @return boolean
      */
     public function isEnabled()
@@ -125,7 +110,7 @@ class Command extends Blueprint
             }
         }
 
-        if ($doctrine || (! $wildfire && $doctrineExists))
+        if (! $wildfire && ($doctrine || $doctrineExists))
         {
             return self::TYPE_DOCTRINE;
         }
